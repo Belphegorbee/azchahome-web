88564385bc3a46894f5d62d65e8cc687598a57c2
Belphegorbee <khanzasam@gmail.com>
2026-02-23 03:46:07 +0800

    Update PWA final: manifest, sw.js, register SW

--- sw.js
+++ sw.js
@@ -3,6 +3,7 @@ const filesToCache = [
   "/",
   "/index.html",
   "/manifest.json",
+  "/sw.js",
   "/assets/menu/loadscreen.jpg",
   "/assets/menu/banner1.jpg",
   "/assets/menu/banner2.jpg",
@@ -17,6 +18,16 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(cacheName).then((cache) => cache.addAll(filesToCache))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => 
+      Promise.all(cacheNames.map((c) => c !== cacheName ? caches.delete(c) : null))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
